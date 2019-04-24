4c147756a40b13762c3c6192179278f07fe9c0c3
Johnathon <johnathon@seesykes.codes>
2019-04-23 21:39:43 -0700

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <h2>Thing</h2>
+    <textarea name="input" id="editor" cols="30" rows="10" v-model="input"></textarea>
+    <div class="preview" v-html='markDown'>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      input: '# hello fellow children  ## welcome to hell.'
+    }
+  },
+  computed: {
+    markDown: function () {
+      marked.setOptions({
+      renderer: new marked.Renderer(),
+      grm: true,
+      breaks: true,
+      sanitize: true,
+      smartypants: true,
+      tables: true
+  })
+  return marked(this.input)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
